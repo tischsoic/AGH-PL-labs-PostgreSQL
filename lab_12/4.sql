4221d9a57410b07829231d5f1ba12b4be8093e7e
Jakub Brzegowski <symfiz@gmail.com>
2017-01-28 00:14:25 +0100

    Implemented solutions to 12.4, 12.5.

--- lab_12/4.sql
+++ lab_12/4.sql
@@ -11,22 +11,33 @@
 -- Informacje o prawach dostępu z poziomu psql można uzyskać za pomocą
 -- polecenia \z (albo \dp). Domyślnie prawa dostępu są tylko dla właściciela!!!
 
+
+-- jesli testujemy u siebie lokalnie na localhost musimy najpierw utworzyc uzytkownika:
+create role testowa login;
+alter role testowa with password 'asdf';
+-- tak się logujemy na tego użytkownika:
+psql -h localhost -p 5432 -d postgres -U testowa
+
+
+
 -- Udziel dostępu do wykonywania zapytań select 2-giej osobie w grupie
 -- do tabeli kompozycje (schemat kwiaciarnia); przetestuj.
+grant select on kompozycje to testowa;
 
 
 -- Udziel dostępu do wykonywania zapytań select 2-giej osobie w grupie
 -- do schematu firma; przetestuj.
-
+grant usage on schema testy to testowa;
+grant select on all tables in schema testy to testowa;
 
 
 -- Udziel dostępu do dodawania, usuwania, modyfikacji wierszy 2-giej osobie
 -- w grupie do tabeli klienci (schemat kwiaciarnia); przetestuj.
-
+grant insert, delete, update on klienci to testowa;
 
 
 -- Zezwól 2-giej osobie na tworzenie nowych obiektów w schemacie firma; przetestuj.
-
+grant create on schema testy to testowa;
 
 
 -- Powtórz ćwiczenie zamieniając się rolami.
@@ -34,3 +45,8 @@
 
 
 -- Usuń przyznane prawa. Sprawdź, czy 2-ga osoba może teraz wykonać jakieś operacje.
+revoke create on schema testy from testowa;
+revoke insert, delete, update on klienci from testowa;
+revoke usage on schema testy from testowa;
+revoke select on all tables in schema testy from testowa;
+revoke select on kompozycje from testowa;
--- lab_12/5.sql
+++ lab_12/5.sql
@@ -2,7 +2,12 @@
 -- datę realizacji, nazwę i adres klienta dla każdego zamówienia - zapytanie takie
 -- może być używane np. przez dział wysyłki.
 
+create view zamowienia_z_klientami as
+    select idzamowienia, datarealizacji, nazwa, ulica || miejscowosc
+    from zamowienia
+    natural join klienci;
 
 
 -- Udostępnij widok z poprzedniego zadania innemu użytkownikowi do odczytu
 -- (pracownikowi działu zamówień ;))
+grant select on zamowienia_z_klientami to testowa;
